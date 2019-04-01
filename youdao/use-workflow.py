e61c9c26efd676b7419fab6dd7eb87868cd68e25
杨帆 <fan.yang19@ele.me>
2019-04-02 01:17:51 +0800

    fix 翻译语句中有引号问题‘

--- youdao/use-workflow.py
+++ youdao/use-workflow.py
@@ -5,12 +5,17 @@
 Author:   Jimu Yang
 Contact:  17621660286@163.com
 这是一个python2脚本 import workflow以及调用 yd-translate来工作
+1. ascii cannot decote byte... 将默认编码设置为utf-8
+2. 不要瞎写print alfred读取的就是sys.stdout
 '''
 
 from workflow import Workflow, ICON_INFO
 import sys
 import subprocess
 import json
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 def call_yd_translate(src):
@@ -29,22 +34,27 @@ def main(wf):
     # 去掉参数两边的空格
     # param = (wf.args[0] if len(wf.args) else '').strip()
     # print wf.args[0]
+
     trans_results = call_yd_translate(wf.args[0])
+    # trans_results = call_yd_translate('找零')
 
-    # trans_results = call_yd_translate('sex')
     # print type(trans_results)
+    # print trans_results
+    arr = trans_results.strip()[1:-1].split(',')
+    # print arr
 
-    json_wrapper = ("{\"result\":" + trans_results + "}").replace("\'", "\"")
-    jo = json.loads(json_wrapper)
+    # json_wrapper = ("{\"result\":" + trans_results + "}").replace("\'", "\"")
+    # jo = json.loads(json_wrapper)
 
-    for result in jo['result']:
-        # print result
-        wf.add_item(title=result, subtitle='...', valid=True, icon=ICON_INFO)
+    for result in arr:
+        result = result.strip()[1:-1]
+        wf.add_item(title=result,
+                    subtitle='...', valid=True, icon=ICON_INFO)
 
     wf.send_feedback()
 
 
-if __name__ == u'__main__':
+if __name__ == '__main__':
     wf = Workflow()
     sys.exit(wf.run(main))
 
